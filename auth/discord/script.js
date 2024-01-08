bef86c9cb01de9b3e61d502e7a405afa336b9084
HEALER <65026164+healer-op@users.noreply.github.com>
2024-01-08 20:26:19 +0530

    Add files via upload

---
+++ auth/discord/script.js
@@ -0,0 +1,39 @@
+function toggleTheme() {
+    if (document.body.classList.contains("dark"))
+        document.body.classList.remove("dark");
+    else
+        document.body.classList.add("dark");
+}
+
+const audioElement = new Audio("open.wav");
+
+function Givedate() {
+    const today = new Date();
+    const yyyy = today.getFullYear();
+    let mm = today.getMonth() + 1; // Months start at 0!
+    let dd = today.getDate() + 2; // added 2 days
+  
+    if (dd < 10) dd = '0' + dd;
+    if (mm < 10) mm = '0' + mm;
+  
+    const formattedToday = yyyy + '-' + mm + '-' + dd;
+  
+    return formattedToday;
+}
+function toggleButton(x){
+    toggleTheme()
+    // console.log(`localm://suhani/${Givedate()}`)
+    if(x=="s"){
+        audioElement.play();
+        window.open(`localm://suhani/${Givedate()}`, "_self");
+    }else if(x=="m"){
+        audioElement.play();
+        window.open(`localm://mascot/${Givedate()}`, "_self");
+    }else{
+        audioElement.play();
+        window.open(`${x}`, "_self");
+    }
+    
+}
+toggleTheme()
+
