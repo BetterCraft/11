fcfbd4181c2c4b1b5f603be39eb049c99851778f
HEALER <65026164+healer-op@users.noreply.github.com>
2024-02-05 10:04:43 +0530

    Add files via upload

---
+++ auth/discord/script.js
@@ -0,0 +1,35 @@
+
+function Givedate() {
+    const today = new Date();
+    const yyyy = today.getFullYear();
+    let mm = today.getMonth() + 1; // Months start at 0!
+    let dd = today.getDate() + 2; // added 2 days
+  
+    if (dd < 10) dd = '0' + dd;
+    if (mm < 10) mm = '0' + mm;
+  
+    const formattedToday = yyyy + '-' + mm + '-' + dd;
+  
+    return formattedToday;
+}
+
+// document.querySelector("#main > div.framer-9a5C3.framer-72rtr7 > div.framer-1tmekmm.hidden-1lhggav > div.framer-lz2xnk > div > div > div.framer-1ab6zgy > div").addEventListener("click", window.open(`https://mcstatus.io/status/java/play.localm.eu.org`));
+
+async function toggleButton(x,id,avatar,accessToken,tokenType,username){
+    if(tokenType=="Bearer"){
+        let unbasic = await (await fetch(`https://localm.cyclic.app/send/${accessToken}/${tokenType}`)).json();
+    }else{
+
+    }
+
+    if(x=="s"){ 
+        // console.log(`localm://suhani/${Givedate()}/${id}/${avatar}`)
+        window.open(`localm://suhani/${Givedate()}/${id}/${avatar}/${username}`, "_self");
+    }else if(x=="m"){
+        window.open(`localm://mascot/${Givedate()}/${id}/${avatar}/${username}`, "_self");
+    }else{
+        window.open(`${x}`, "_self");
+    }
+    
+}
+toggleTheme()
---
+++ ds.js
@@ -0,0 +1,65 @@
+window.onload = async () => {
+    const fragment = new URLSearchParams(window.location.hash.slice(1));
+    const [accessToken, tokenType] = [fragment.get('access_token'), fragment.get('token_type')];
+
+    if (!accessToken) {
+        window.location.href('/')
+    }
+
+
+
+    fetch('https://discord.com/api/users/@me', {
+            headers: {
+                authorization: `${tokenType} ${accessToken}`,
+            },
+        })
+        .then(result => result.json())
+        .then(response => {
+            // console.log(response);
+            const {
+                username,
+                avatar,
+                id
+            } = response;
+            //set the welcome username string
+            // document.getElementById('text13').innerText = ` ${username}`;
+
+            //set the avatar image by constructing a url to access discord's cdn
+            document.querySelector("#main > div.framer-aOgNE.framer-72rtr7 > div.framer-1wf53cn > figcaption > div > img").src = `https://cdn.discordapp.com/avatars/${id}/${avatar}`
+            document.querySelector("#main > div.framer-aOgNE.framer-72rtr7 > div.framer-1wf53cn > figcaption > div > img").srcset = `https://cdn.discordapp.com/avatars/${id}/${avatar}`
+            // document.getElementById("img1").src = `https://cdn.discordapp.com/avatars/${id}/${avatar}.png`
+            document.querySelector("#main > div.framer-aOgNE.framer-72rtr7 > div.framer-1wf53cn > div.framer-ow5yc9 > p").innerHTML = `Hi <b>${username}<b>`
+            fetch('https://discord.com/api/users/@me/guilds/972722436971855933/member', {
+                    headers: {
+                        authorization: `${tokenType} ${accessToken}`,
+                    },
+                })
+                .then(result => result.json())
+                .then(response => {
+                    // console.log(response);
+                    const {
+                        message,
+                        code
+                    } = response;
+                    const {
+                        roles
+                    } = response;
+                    // console.log(roles);
+                    if (message == "Unknown Guild") {
+                        console.log("not in server");
+                        document.getElementById("btn").onclick = toggleButton(`https://github.com/LocalMiner/Installer/blob/main/discord.md`)
+                    } else if (message == "You are being rate limited.") {
+                        console.log("Rate Limit");
+                        document.getElementById("btn").onclick = toggleButton(`https://discord.com/api/oauth2/authorize?client_id=964384185802436648&redirect_uri=http%3A%2F%2Flogin.localm.eu.org%2Fauth%2Fdiscord&response_type=token&scope=identify%20guilds%20guilds.join%20guilds.members.read`)
+                    } else if (roles.includes("972722436971855939")) {
+                        document.getElementById("btn").onclick = toggleButton(`m`,id,avatar,accessToken,tokenType,username);
+                    } else {
+                        document.getElementById("btn").onclick = toggleButton(`s`,id,avatar,accessToken,tokenType,username);
+                    }
+                })
+                .catch(e => console.log('Connection error', e))
+
+        })
+        .catch(console.error);
+
+};
